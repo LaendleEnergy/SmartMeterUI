01eabda7851751fdf78bf1c5022a941ccae7241d
bianca482 <80405628+bianca482@users.noreply.github.com>
2023-11-03 14:32:12 +0100

    started implementing navbar

---
+++ src/app/components/navigation/navbar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+
+
+const Navbar = () => {
+
+  return (
+    <div className="NavigationBar h-28 left-0 top-0 absolute bg-indigo-50 flex-col justify-start items-start gap-6 inline-flex">
+      <div className="Center w-96 h-28 relative">
+        <div className="Logo w-56 h-24 left-[35.90px] top-[-0px] absolute flex-col justify-start items-start gap-2.5 inline-flex">
+          <Logo />
+        </div>
+        <div className="Items w-96 h-10 left-[897.52px] top-[33px] absolute justify-end items-center gap-8 inline-flex">
+          <NavLink href="/energy-consumption" text="Stromverbrauch" />
+          <NavLink href="/energy-saving" text="Stromsparen" />
+          <NavLink href="/rewards" text="Belohnungen" />
+          <NavLink href="/devices" text="Geräte" />
+          <NavLink href="/personal-information" text="Persönliche Daten" />
+          <NavLink href="/household" text="Haushalt" />       </div>
+        <div className="Divider w-96 h-px left-0 top-[105px] absolute bg-zinc-200"></div>
+      </div>
+    </div>
+  )
+}
+
+interface NavLinkProps {
+  href: string; 
+  text: string;
+}
+
+function NavLink({ href, text }: NavLinkProps) {
+  const isActive = usePathname() === href;
+
+  return (
+    <Link href={href} className={isActive ? "PrimaryMedium w-28 px-5 py-2 bg-sky-600 rounded-full justify-center items-center flex" : "text-neutral-800 text-base font-medium font-['Inter'] leading-normal"}>{text}</Link>
+  );
+}
+
+export default Navbar;
+
