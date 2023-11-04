766db506da3c9be75d368a5bfcc6fa7103bfef2f
bianca482 <80405628+bianca482@users.noreply.github.com>
2023-11-04 16:36:06 +0100

    added input component

---
+++ src/app/components/InputAttribute.tsx
@@ -0,0 +1,17 @@
+interface Input {
+    name: string;
+    type: string;
+}
+
+export default function InputAttribute({ name, type }: Input) {
+    return (
+        <div className="justify-start items-start flex">
+            <div className="p-2 rounded border-4 border-black justify-start items-center flex">
+                <input type={type} id={name} name={name} placeholder={name} className="text-zinc-600 p-4" />
+            </div>
+        </div>
+    )
+}
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,20 @@
 import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
 import Logo from "./components/Logo";
+import InputAttribute from "@/app/components/InputAttribute";
 
 export default function Home() {
 
   return (
     <div className="Welcome p-[5%]">
       <div className="RegisterOrLogin flex-col justify-start items-center gap-8 inline-flex">
-      <Logo h={388} w={740}></Logo>
+        <Logo h={388} w={740}></Logo>
         <div className="WelcomePhrase p-2.5 flex justify-start items-center">
           <div className="WillkommenBeiLaendleenergy text-4xl font-bold ">Willkommen bei LaendleEnergy!</div>
         </div>
         <div className="Text text-lg">Registriere dich jetzt, um den Stromverbrauch deines Haushaltes und die damit verbundenen Kosten beobachten und Feedback über die Energieeffizienz deiner Geräte erhalten zu können.</div>
-
-        <div className="Email justify-start items-start flex">
-          <div className="Email p-2 rounded border-4 border-black justify-start items-center flex">
-            <input type="text" id="email" name="email" placeholder="Email" className="Email text-zinc-600 p-4" />
-          </div>
-        </div>
-        <div className="Passwort justify-start items-start flex">
-          <div className="Passwort p-2 rounded border-4 border-black justify-start items-center flex">
-            <input type="password" id="password" name="password" placeholder='Passwort' className="Passwort text-zinc-600 p-4" />
-          </div>
-        </div>
+        <InputAttribute name="Email" type="text"></InputAttribute>
+        <InputAttribute name="Password" type="password"></InputAttribute>
         <div className="ActiveButton w-42 px-8 py-4 bg-primary-600 rounded-full justify-center items-center flex">
           <Link href="./pages/energy-consumption"><span className="Text text-center text-white text-base font-medium leading-normal">Log In</span></Link>
         </div>
