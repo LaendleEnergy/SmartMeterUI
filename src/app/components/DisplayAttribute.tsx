05741d10cf14f31500bc4f5c3ba85f5ba110cf63
bianca482 <80405628+bianca482@users.noreply.github.com>
2023-11-08 18:38:37 +0100

    rewards ui

--- src/app/components/DisplayAttribute.tsx
+++ src/app/components/DisplayAttribute.tsx
@@ -1,11 +1,15 @@
+import { FaStar } from "react-icons/fa";
+
 interface DisplayAttribute {
     name: string;
+    star?: boolean;
 }
 
-export default function DisplayAttribute({ name }: DisplayAttribute) {
+export default function DisplayAttribute({ name, star = false }: DisplayAttribute) {
     return (
-        <div className="w-96 h-14 p-3 rounded border-4 border-black justify-start items-center inline-flex">
+        <div className="w-96 h-14 p-3 space-x-3 rounded border-4 border-black justify-start items-center inline-flex">
             <div className="text-zinc-600 text-base tracking-wider">{name}</div>
+            <FaStar class={star ? "text-right" : "hidden"}></FaStar>
         </div>
     )
 }
--- src/app/pages/rewards/page.tsx
+++ src/app/pages/rewards/page.tsx
@@ -1,59 +1,35 @@
+import DisplayAttribute from "@/app/components/DisplayAttribute";
 import Navigation from "../../components/navigation/NavBar";
 import Link from "next/link";
+import { FaEdit } from "react-icons/fa";
 
 export default function Rewards() {
     return (
-        <div className="Rewards w-96 h-96 bg-white flex-col justify-start items-start gap-2.5 inline-flex">
+        <div className="Rewards">
             <header><Navigation /></header>
-            <div className="Page w-96 h-96 relative">
-                <div className="Background w-96 h-56 pl-96 pr-7 pt-52 left-[492px] top-[211px]   bg-indigo-50 rounded-sm border-2 border-black justify-end items-center inline-flex">
-                    <div className="Icon p-3 bg-white bg-opacity-0 rounded-lg justify-start items-start inline-flex">
-                        <div className="IconsMediumStar w-6 h-6 relative" />
+            <div className="flex flex-col justify-center items-center space-y-10 py-[10%]">
+                <div className="text-4xl font-bold">Belohnungen</div>
+                <div className="space-y-2 bg-indigo-50 rounded-sm border-2 border-black p-4">
+                    <div className="CurrentReward text-3xl font-bold text-center">Aktuelle Belohnung</div>
+                    <div className="Edit justify-center items-center inline-flex space-x-3">
+                        <div className="Bearbeiten text-xl">Bearbeiten</div>
+                        <FaEdit></FaEdit>
                     </div>
+                    <div className="Incentive text-xl font-bold text-center">Gratis Pizza</div>
+                    <div className="text-lg text-right">noch 5 Tage</div>
                 </div>
-                <div className="Competition left-[653px] top-[125px]     text-4xl font-bold  ">Belohnungen</div>
-                <div className="CurrentReward w-80 left-[595px] top-[229px]  "><span className="  text-3xl font-bold  ">Aktuelle Belohnung</span><span className="  text-2xl font-medium  "> </span></div>
-                <div className="Incentive w-40 left-[683px] top-[363px]     text-2xl font-bold  ">Gratis Pizza</div>
-                <div className="Leaderboard w-96 h-96 left-[434px] top-[487px]   bg-indigo-50 rounded-sm border-2 border-black">
-                    <div className="Name left-[134px] top-[108px]   justify-start items-start inline-flex">
-                        <div className="Name justify-start items-start flex">
-                            <div className="Name w-96 h-14 pl-3.5 pr-2.5 py-2.5 rounded border-4 border-black justify-start items-center gap-2.5 flex">
-                                <div className="Name text-zinc-600 text-base     tracking-wider">Name 1 - Anzahl der Labels</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="Name left-[134px] top-[197px]   justify-start items-start inline-flex">
-                        <div className="Name w-96 h-14 pl-3.5 pr-2.5 py-2.5 rounded border-4 border-black justify-start items-center gap-2.5 flex">
-                            <div className="Name text-zinc-600 text-base     tracking-wider">Name 2 - Anzahl der Labels</div>
-                        </div>
-                    </div>
-                    <div className="Name left-[134px] top-[286px]   justify-start items-start inline-flex">
-                        <div className="Name w-96 h-14 pl-3.5 pr-2.5 py-2.5 rounded border-4 border-black justify-start items-center gap-2.5 flex">
-                            <div className="Name text-zinc-600 text-base     tracking-wider">Name 3 - Anzahl der Labels</div>
-                        </div>
-                    </div>
-                    <div className="Rangliste left-[247px] top-[36px]     text-3xl font-bold   tracking-widest">Rangliste</div>
-                    <div className="Icon p-3 left-[471px] top-[201px]   bg-white bg-opacity-0 rounded-lg justify-start items-start inline-flex">
-                        <div className="IconsMediumStar w-6 h-6 relative" />
-                    </div>
+                <div className="Leaderboard flex flex-col space-y-2 rounded-sm border-2 border-black bg-indigo-50 p-4">
+                    <div className="Rangliste text-3xl font-bold">Rangliste</div>
+                    <DisplayAttribute name="Name 1 - Anzahl der Labels"></DisplayAttribute>
+                    <DisplayAttribute name="Name 2 - Anzahl der Labels" star={true}></DisplayAttribute>
+                    <DisplayAttribute name="Name 3 - Anzahl der Labels"></DisplayAttribute>
                 </div>
-
-                <div className="Edit w-36 h-12 left-[761px] top-[268px]   justify-center items-center inline-flex">
-                    <div className="Bearbeiten   text-xl    ">Bearbeiten</div>
-                    <div className="Icon w-12 h-12 p-3 bg-indigo-50 rounded-lg justify-start items-start inline-flex">
-                        <div className="IconsMediumEdit w-6 h-6 relative">
-                            <div className="IconGrid w-6 h-6 left-0 top-0  " />
-                            <div className="Rotate origin-top-left rotate-45 w-1 h-5 left-[17.66px] top-[3.51px]  ">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="Bearbeiten left-[912px] top-[392px]     text-xl    ">noch 5 Tage</div>
                 <div className="ActiveButton inline-flex justify-center items-center bg-primary-600 rounded-full p-3 space-x-3 transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow">
                     <Link href="../pages/energy-consumption">
                         <span className="Default text-center text-white text-xl font-medium   leading-normal">Jetzt Verbrauch zuordnen</span>
                     </Link>
                 </div>
             </div>
-        </div>)
+        </div>
+    )
 }
