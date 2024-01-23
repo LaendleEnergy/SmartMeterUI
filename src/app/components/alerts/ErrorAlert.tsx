16f7e7956f7314b55a35cb3e64c1eb50371834b3
Dominik <potchamap@gmail.com>
2024-01-23 14:56:22 +0100

    refactored alerts with animation

--- src/app/components/alerts/ErrorAlert.tsx
+++ src/app/components/alerts/ErrorAlert.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './css/ErrorAlert.css'
 
 
 interface ErrorAlertProps {
@@ -9,7 +10,13 @@ export default function ErrorAlert(props: ErrorAlertProps) {
     const errorMessage = props.message;
     return (
         <div className="w-150 h-100 bg-red-100 rounded-lg border-2 border-zinc-400" style={{margin: "1vw"}}>
-            {errorMessage}
+            <svg className="checkmark__red" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                <circle className="checkmark__circle__red" cx="26" cy="26" r="25" fill="red" />
+                <path className="checkmark__check__red" fill="white" d="M16 16 36 36 M36 16 16 36" />
+            </svg>
+            <div style={{margin: "2vw"}}>
+                {errorMessage}
+            </div>
         </div>
     );
 }
--- src/app/components/alerts/SuccessAlert.tsx
+++ src/app/components/alerts/SuccessAlert.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './css/SuccessAlert.scss'
 
 
 interface ErrorAlertProps {
@@ -9,7 +10,10 @@ export default function SuccessAlert(props: ErrorAlertProps) {
     const successMessage = props.message;
     return (
         <div className="w-150 h-100 bg-green-100 rounded-lg border-2 border-zinc-400" style={{margin: "1vw"}}>
-            {successMessage}
+            <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+            <div style={{margin: "2vw"}}>
+                {successMessage}
+            </div>
         </div>
     );
 }
