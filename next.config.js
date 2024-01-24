77d3cf67a0cadd9659eeb818405f41484842a28d
Felix Lahnsteiner <felix.lahnsteiner@outlook.com>
2024-01-24 12:31:43 +0100

    try solve cors

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const withPWA = require("next-pwa")({
   /** @type {import('next').NextConfig} */
   const nextConfig = {
     reactStrictMode: true,
+    async headers() {
+      return [
+          {
+              source: "*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "*" },
+                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+              ]
+          }
+      ]
+  }
   };
   
   module.exports = withPWA(nextConfig);
